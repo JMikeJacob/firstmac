232b65eb53a95e5d960a86fd8043625ad5740b95
Jerome Michael Jacob <jmikejacob98@gmail.com>
2023-01-12 12:27:25 +0800

    Move input data to dedicated folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const data = require('./data.json');
-const input = require('./input.json');
+const data = require('./input/data.json');
+const input = require('./input/input.json');
 const findShortestPath = require('./search');
 
 for (let i = 0; i < input.length; i++) {
